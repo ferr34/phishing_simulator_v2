91a2d7bb26fb2ca4bea87ba4578e4239c50fba9b
Cataldo Ferrante <ferrantealdo194@gmail.com>
2021-07-27 10:53:14 +0200

    Primo commit per phishing_simulatorV2

---
+++ src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Inizio simulazione phishing. Connessione al server..."); // Compilazione OK 
+    }
+}
