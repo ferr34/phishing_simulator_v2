d7056294ab00235cad98a7d3feb1150dee459fff
Cataldo Ferrante <ferrantealdo194@gmail.com>
2021-08-12 15:20:07 +0200

    Secondo commit- aggiunta indentazione su interfaccia da inserire

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Inizio simulazione phishing. Connessione al server..."); // Compilazione OK 
+        System.out.println("Inizio simulazione phishing. Connessione al server Apache in corso..."); // Compilazione OK 
+    
+    // Devo inserire l'interfaccia di Netbeans
+    
+    
     }
+
+     
 }
