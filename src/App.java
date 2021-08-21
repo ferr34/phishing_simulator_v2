3d35307d7e1e1fd785dfe5b380b4093f834c0a27
Cataldo Ferrante <ferrantealdo194@gmail.com>
2021-08-21 19:13:30 +0200

    Aggiunta interfaccia finale, primo prototipo funzionante di invio mail. Da aggiungere l'invio di eventuali allegati.

--- src/App.java
+++ src/App.java
@@ -2,10 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Inizio simulazione phishing. Connessione al server Apache in corso..."); // Compilazione OK 
     
-    // Apertura interfaccia 
+    // Apertura interfaccia principale 
     startPhish sp = new startPhish(); 
-
-
     
     }
 
--- src/startPhish.java
+++ src/startPhish.java
@@ -1,20 +1,29 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Properties;
+
 import javax.swing.*;
-import java.awt.*; 
-import java.awt.event.*;
+
+// Classi per l'invio della mail 
+import javax.mail.*; 
+import javax.mail.internet.*;
+
 
 public class startPhish extends JFrame
 { 
     public static JFrame f; 
 
     // Costruttore superclasse 
-    public startPhish()
+    public startPhish() throws MessagingException
     {
-        super("try_phish_me v 1.0"); 
+        super("try_phish_me v1.0"); 
         this.setVisible(true);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         this.setResizable(false);
-        this.setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/img/tpm_logo.jpg")));
+        this.setPreferredSize(new java.awt.Dimension(450,501));
+        this.setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/img/amo5.jpg")));
 
         // Prendo il container 
         Container c = this.getContentPane(); 
@@ -65,19 +74,19 @@ public class startPhish extends JFrame
         // Label & buttons
         setPreferredSize(new java.awt.Dimension(450, 501));
 
-        title.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        title.setFont(new java.awt.Font("Consolas", 0, 18)); // NOI18N
         title.setText("Inserire dati della vittima");
         title.setForeground(Color.WHITE);
 
-        email_label.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        email_label.setFont(new java.awt.Font("Consolas", 0, 14)); // NOI18N
         email_label.setText("E - mail");
         email_label.setForeground(Color.WHITE);
 
-        object_label.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        object_label.setFont(new java.awt.Font("Consolas", 0, 14)); // NOI18N
         object_label.setText("Oggetto");
         object_label.setForeground(Color.WHITE);
 
-        testo_label.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        testo_label.setFont(new java.awt.Font("Consolas", 0, 14)); // NOI18N
         testo_label.setText("Testo e - mail");
         testo_label.setForeground(Color.WHITE);
 
@@ -85,13 +94,17 @@ public class startPhish extends JFrame
         testo_ta.setRows(5);
         jScrollPane2.setViewportView(testo_ta);
 
-        upload_attachments.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        upload_attachments.setText("Carica allegati ");
+        upload_attachments.setFont(new java.awt.Font("Consolas", 0, 12)); // NOI18N
+        upload_attachments.setText("Carica allegati");
          
-        lets_phish_btn.setText("amo");
+        lets_phish_btn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/insertRight.png")));
+        lets_phish_btn.setContentAreaFilled(false);
+        lets_phish_btn.setBorderPainted(false);
+        lets_phish_btn.setMaximumSize(new java.awt.Dimension(32,32));
+        lets_phish_btn.setMinimumSize(new java.awt.Dimension(32,32));
 
-        lets_phish_label.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        lets_phish_label.setText("lets_phish");
+        lets_phish_label.setFont(new java.awt.Font("Consolas", 0, 14)); // NOI18N
+        lets_phish_label.setText("Attacca!");
         lets_phish_label.setForeground(Color.WHITE);
 
         // Layout code - Inherited from NetBeans 
@@ -101,35 +114,31 @@ public class startPhish extends JFrame
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(30, 30, 30)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(testo_label)
-                    .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(testo_label, javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(email_label, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(object_label))
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(28, 28, 28)
-                                .addComponent(title))
-                            .addGroup(layout.createSequentialGroup()
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(email_tf, javax.swing.GroupLayout.DEFAULT_SIZE, 306, Short.MAX_VALUE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(obj_tf))))
-                    .addComponent(jScrollPane2))
-                .addContainerGap(31, Short.MAX_VALUE))
+                            .addComponent(object_label)
+                            .addComponent(email_label))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(email_tf, javax.swing.GroupLayout.PREFERRED_SIZE, 306, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(obj_tf, javax.swing.GroupLayout.PREFERRED_SIZE, 306, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(lets_phish_label)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(lets_phish_btn, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(19, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(lets_phish_label)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(lets_phish_btn, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(16, 16, 16))
+                        .addComponent(title)
+                        .addGap(78, 78, 78))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(upload_attachments)
-                        .addGap(156, 156, 156))))
+                        .addGap(146, 146, 146))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -146,19 +155,114 @@ public class startPhish extends JFrame
                     .addComponent(obj_tf, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(testo_label)
-                .addGap(18, 18, 18)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(upload_attachments)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 16, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lets_phish_btn)
-                    .addComponent(lets_phish_label))
-                .addGap(17, 17, 17))
+                .addGap(20, 20, 20)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(upload_attachments, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(35, 35, 35)
+                        .addComponent(lets_phish_btn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(lets_phish_label)
+                        .addGap(10, 10, 10)))
+                .addContainerGap(36, Short.MAX_VALUE))
         );
 
         pack();
 
+        lets_phish_btn.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                // Invia l'email per l'attacco 
+                System.out.println("Pulsante premuto!");
+                try
+                {
+                    // Impostazione dati dell'attacco 
+                    boolean debug = false; 
 
+                    // Impostazioni SMTP
+                    Properties props = new Properties();
+                    props.put("mail.smtp.host","smtp.gmail.com");
+                    props.put("mail.smtp.port", "587");
+                    props.put("mail.smtp.starttls.enable", "true");
+                    props.put("mail.smtp.auth", "true"); 
+
+                    // Istanzio un oggetto in Session 
+                    Session session = Session.getDefaultInstance(props,   new javax.mail.Authenticator() {  
+                        protected PasswordAuthentication getPasswordAuthentication() {  
+                        return new PasswordAuthentication("segreteriastudenti.test@gmail.com","Segreteria44!");  
+                        }  
+                       });
+                    session.setDebug(debug);
+
+                    // Creo l'oggetto Message partendo da session 
+                    Message msg = new MimeMessage(session);
+
+                    // Definisco il mittente 
+                    InternetAddress email_mitt = new InternetAddress("Segreteria_Studenti");
+                    msg.setFrom(email_mitt);
+
+                    // Definisco il destinatario 
+                    InternetAddress email_dest = new InternetAddress(email_tf.getText().toString()); 
+                    msg.setRecipient(Message.RecipientType.TO, email_dest);
+
+                    // Definisco header aggiuntivi
+                    String oggettoEmail = obj_tf.getText().toString();
+        
+                    if(oggettoEmail != null)
+                         {
+                            msg.setSubject(oggettoEmail);
+                         }
+                        else 
+                        {
+                            // Intestazione per frode di base 
+                            msg.setSubject("Pericolo intrusione nel tuo account del Politecnico");
+                        }
+        
+
+                        // Definisco il messaggio da inviare e setto il contenuto -> controllo che non sia vuoto, solo per test 
+        
+                        String daInviare = " ";
+
+                    if(testo_ta.getText().toString() != null)
+                        {
+                            daInviare = testo_ta.getText().toString();
+                        }
+                        else 
+                        {     
+                            daInviare = "Gentile studente/studentessa, \n è stata rilevata una fuga di password, per cortesia reinserisci le ultime credenziali a questo link: \n\n" +
+                                "Se hai problemi non esitare a contattarci. \n Cordiali saluti, la Segreteria Studenti"; 
+                        }
+
+        
+                        msg.setContent(daInviare, "text/plain");
+
+                        // Invio del messaggio 
+                        Transport.send(msg); 
+                        
+                        // Message box per indicare che è stato correttamente inviato il messaggio + blank tf per ricominciare 
+                        
+                }
+                catch(AddressException e1)
+                {
+                    System.out.println("Address exception rilevata\n");
+                    e1.printStackTrace(); 
+                }
+                catch(MessagingException e2)
+                {
+                    System.out.println("MessagingException rilevata \n");
+                    e2.printStackTrace();
+                }
+
+      
+
+
+            }
+        });
+        
     }
+
+
 }
